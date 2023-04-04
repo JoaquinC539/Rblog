4f6a8675bb8b383ad8f67e4f8a65e37536c9e02d
JoaquinC539 <cjoaquinangel@gmail.com>
2023-04-04 14:04:21 -0600

    Made functional routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
-
+import Layout from './Layout';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import Missing from './Missing';
+import About from './About';
+import {Route,Routes,useNavigate} from 'react-router-dom';
+import { useEffect,useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+  
+  <Routes> 
+    <Route path='/' element={<Layout />} >   
+    <Route index element={<Home />} /> 
+    <Route path='post'>
+      <Route index element={<NewPost />} />
+      <Route path=':id' element={<PostPage />} />
+    </Route>
+    <Route path="about" element={<About />} />
+    <Route path="*" element={<Missing />} />
+    
+    </Route>
+  </Routes>  
+     
+  
+    
   );
 }
 
