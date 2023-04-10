47256af6afa9b08c3184caacdf0ac058192283f0
JoaquinC539 <cjoaquinangel@gmail.com>
2023-04-10 15:48:24 -0600

    Added custom an axios hooks

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import {Route,Routes,useNavigate} from 'react-router-dom';
 import { useEffect,useState } from 'react';
 import { format } from 'date-fns';
 import api from './api/posts';
+import useWindowSize from './hooks/useWindowSize';
+import useAxiosFetch from './hooks/useAxiosFetch';
+
 function App() {
   const [posts, setPosts]=useState([]);
 const [search,setSearch]=useState("");
@@ -18,24 +21,12 @@ const [postBody,setPostBody]=useState('');
 const navigate=useNavigate();
 const [editTitle,setEditTitle]=useState('');
 const [editBody,setEditBody]=useState('');
+const {width}=useWindowSize();
+const {data,fetchError,isLoading}=useAxiosFetch('http://localhost:3500/posts');
+ 
 useEffect(()=>{
-  const fetchPosts=async ()=>{
-    try{
-      const response=await api.get('/posts');
-      if (response && response.data) setPosts(response.data);
-    }catch(err){
-      if(err.response){
-      console.log(err.response.data);
-      console.log(err.response.status);
-      console.log(err.response.headers); 
-      }else{
-        console.log(`Error: ${err.message}`); 
-      }
-    }
-  }
-  fetchPosts();
-  
-},[])
+  setPosts(data)
+},[data])
 
 const handleSubmit=async (e)=>{
   e.preventDefault();
@@ -86,8 +77,8 @@ const handleEdit=async (id)=>{
 }
   return (  
   <Routes> 
-    <Route path='/' element={<Layout search={search} setSearch={setSearch}/>} >   
-    <Route index element={<Home posts={searchResults}/>} /> 
+    <Route path='/' element={<Layout search={search} setSearch={setSearch} width={width}/>} >   
+    <Route index element={<Home posts={searchResults} fetchError={fetchError} isLoading={isLoading}/>} /> 
     <Route path='post'>
       <Route index element={<NewPost handleSubmit={handleSubmit} postTitle={postTitle} setPostTitle={setPostTitle} postBody={postBody} setPostBody={setPostBody} />} />
       <Route path=':id' element={<PostPage posts={posts} handleDelete={handleDelete}/>} />
