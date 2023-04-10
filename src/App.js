2faac82dd3f50c16e4caa8e4932c8d3323e8798a
JoaquinC539 <cjoaquinangel@gmail.com>
2023-04-10 14:41:00 -0600

    Made the posts on a db

--- src/App.js
+++ src/App.js
@@ -4,61 +4,86 @@ import NewPost from './NewPost';
 import PostPage from './PostPage';
 import Missing from './Missing';
 import About from './About';
+import EditPost from './EditPost';
 import {Route,Routes,useNavigate} from 'react-router-dom';
 import { useEffect,useState } from 'react';
 import { format } from 'date-fns';
+import api from './api/posts';
 function App() {
-  const [posts, setPosts]=useState([{
-    id: 1,
-    title: "My First Post",
-    datetime: "July 01, 2021 11:17:36 AM",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-  },
-  {
-    id: 2,
-    title: "My 2nd Post",
-    datetime: "July 01, 2021 11:17:36 AM",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-  },
-  {
-    id: 3,
-    title: "My 3rd Post",
-    datetime: "July 01, 2021 11:17:36 AM",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-  },
-  {
-    id: 4,
-    title: "My Fourth Post",
-    datetime: "July 01, 2021 11:17:36 AM",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-  }])
+  const [posts, setPosts]=useState([]);
 const [search,setSearch]=useState("");
 const [searchResults,setSearchResults]=useState([]);
 const [postTitle,setPostTitle]=useState('');
 const [postBody,setPostBody]=useState('');
 const navigate=useNavigate();
+const [editTitle,setEditTitle]=useState('');
+const [editBody,setEditBody]=useState('');
+useEffect(()=>{
+  const fetchPosts=async ()=>{
+    try{
+      const response=await api.get('/posts');
+      if (response && response.data) setPosts(response.data);
+    }catch(err){
+      if(err.response){
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers); 
+      }else{
+        console.log(`Error: ${err.message}`); 
+      }
+    }
+  }
+  fetchPosts();
+  
+},[])
 
-const handleSubmit=(e)=>{
+const handleSubmit=async (e)=>{
   e.preventDefault();
   const id=posts.length?posts[posts.length-1].id+1:1;
   const datetime=format(new Date(), 'MMMM dd, yyyy pp');
-  const newPost={id:id, title:postTitle,datetime:datetime,body:postBody}
-  const allPost=[...posts,newPost];
+  const newPost={id:id, title:postTitle,datetime:datetime,body:postBody};
+  try{
+  const response =await api.post('/posts',newPost);    
+  const allPost=[...posts,response.data];
   setPosts(allPost);
   setPostTitle("");
   setPostBody("");
   navigate('/'); 
+  }catch(err){
+    if(err.response){
+      console.log(`Error: ${err}`);
+    }
+  }
 }
-const handleDelete=(id)=>{
+const handleDelete= async (id)=>{
+  try{
+    await api.delete('/posts/'+id)
   const postList=posts.filter(post=>post.id!==id);
   setPosts(postList);
   navigate('/');
+  }catch(err){
+    console.log(`Error: ${err.message}`); 
+  }
 }
 useEffect(()=>{
 const filteredResults=posts.filter(post=>((post.body).toLowerCase()).includes(search.toLowerCase())
 || ((post.title).toLowerCase()).includes(search.toLowerCase()));
 setSearchResults(filteredResults.reverse());
 },[posts,search])
+
+const handleEdit=async (id)=>{
+  const datetime=format(new Date(), 'MMMM dd, yyyy pp');
+  const updatedPost={id:id, title:editTitle,datetime:datetime,body:editBody};
+  try{
+    const response=await api.put('/posts/'+id,updatedPost);
+    setPosts(posts.map(post=>post.id===id?{...response.data}:post));
+    setEditTitle('');
+    setEditBody('');
+    navigate('/');
+  }catch(err){
+    console.log(err)
+  }
+}
   return (  
   <Routes> 
     <Route path='/' element={<Layout search={search} setSearch={setSearch}/>} >   
@@ -66,6 +91,9 @@ setSearchResults(filteredResults.reverse());
     <Route path='post'>
       <Route index element={<NewPost handleSubmit={handleSubmit} postTitle={postTitle} setPostTitle={setPostTitle} postBody={postBody} setPostBody={setPostBody} />} />
       <Route path=':id' element={<PostPage posts={posts} handleDelete={handleDelete}/>} />
+    </Route>
+    <Route path='edit'>
+      <Route path=':id'element={<EditPost posts={posts} handleEdit={handleEdit} editTitle={editTitle} setEditTitle={setEditTitle} editBody={editBody} setEditBody={setEditBody}/>}></Route>
     </Route>
     <Route path="about" element={<About />} />
     <Route path="*" element={<Missing />} />
--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ const Home = ({posts}) => {
         {posts.length?(
           <Feed posts={posts} />
         ):(
-          <p style={{marginTop:"2rem"}}>No posts to displaye</p>
+          <p style={{marginTop:"2rem"}}>No posts to display</p>
         )}
       </main>
     )
